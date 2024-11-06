ecc695af0bbb6410150338b68fd700a7bb5685b8
Sparkle <rachmillie@yahoo.com>
2024-11-06 17:08:16 -0500

    feature list in main file

--- Main.java
+++ Main.java
@@ -1,4 +1,14 @@
 public class Main {
+    /*
+    Features!
+    Tell how to care for plants:
+        Soil type
+        Watering
+        Sunlight
+        If plant is healthy or not
+        Other factors?
+    Tell if plant is harmful
+     */
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("Test");
